1174af4621a9590c95c6a9742ca36ab9fc4f298b
Capu <mariokapu@gmail.com>
2015-08-21 18:55:39 +0200

    Changed theme to use colors #9cf and #4b9cd4.

--- src/app/app.theme.js
+++ src/app/app.theme.js
@@ -1,6 +1,14 @@
 angular.module('tf2stadium')
 .config(function($mdThemingProvider) {
+  var darkBlueMap = $mdThemingProvider.extendPalette('light-blue', {
+    '500': '4b9cd4'
+  });
+  var lightBlueMap = $mdThemingProvider.extendPalette('light-blue', {
+    '500': '99ccff'
+  });
+  $mdThemingProvider.definePalette('darkBluePalette', darkBlueMap);
+  $mdThemingProvider.definePalette('lightBluePalette', lightBlueMap);
   $mdThemingProvider.theme('default')
-    .primaryPalette('light-blue')
-    .accentPalette('deep-purple');
+    .primaryPalette('darkBluePalette')
+    .accentPalette('lightBluePalette');
 });
