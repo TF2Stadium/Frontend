0d146a7774464b443e91443c9cb890e1ccff018f
Capu <mariokapu@gmail.com>
2015-08-29 23:38:59 +0200

    Added theme service and controller.

--- src/app/app.theme.js
+++ src/app/app.theme.js
@@ -1,14 +1,53 @@
-angular.module('tf2stadium')
-.config(function($mdThemingProvider) {
-  var darkBlueMap = $mdThemingProvider.extendPalette('light-blue', {
-    '500': '4b9cd4'
+(function() {
+  'use strict';
+  
+  angular.module('tf2stadium')
+  .config(function($mdThemingProvider) {
+    var darkBlueMap = $mdThemingProvider.extendPalette('light-blue', {
+      '500': '4b9cd4'
+    });
+    var lightBlueMap = $mdThemingProvider.extendPalette('light-blue', {
+      '500': '99ccff'
+    });
+    $mdThemingProvider.definePalette('darkBluePalette', darkBlueMap);
+    $mdThemingProvider.definePalette('lightBluePalette', lightBlueMap);
+    $mdThemingProvider.theme('default')
+      .primaryPalette('darkBluePalette')
+      .accentPalette('lightBluePalette');
   });
-  var lightBlueMap = $mdThemingProvider.extendPalette('light-blue', {
-    '500': '99ccff'
+
+  angular.module('tf2stadium').factory('themeService', function() {
+
+    var themes = [
+      {name: "TF2Stadium", selector: "default-theme"},
+      {name: "TF2Stadium Dark", selector: "dark-theme"}
+    ]
+
+    var currentTheme = themes[0];
+
+    return {
+      getThemes: function() {
+          return themes;
+        },
+      currentTheme: currentTheme
+    };
+
   });
-  $mdThemingProvider.definePalette('darkBluePalette', darkBlueMap);
-  $mdThemingProvider.definePalette('lightBluePalette', lightBlueMap);
-  $mdThemingProvider.theme('default')
-    .primaryPalette('darkBluePalette')
-    .accentPalette('lightBluePalette');
-});
+
+  angular.module('tf2stadium')
+  .controller('ThemeController', ['$scope', 'themeService', function($scope, themeService) {
+
+    console.log ('Starting ThemeController');
+    $scope.themes = themeService.getThemes();
+    $scope.currentTheme = themeService.currentTheme;
+
+    $scope.setCurrentTheme = function(theme) {
+      themeService.currentTheme = theme;
+    }
+    $scope.getCurrentTheme = function() {
+      return themeService.currentTheme;
+    }
+
+  }]);
+
+})();
