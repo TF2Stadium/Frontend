d93cd5e25819b83fceaa64af9f94c9da9d10c5e1
Capu <mariokapu@gmail.com>
2015-08-30 23:54:24 +0200

    Renamed service & added watcher to controller.

--- src/app/app.theme.js
+++ src/app/app.theme.js
@@ -16,7 +16,7 @@
       .accentPalette('lightBluePalette');
   });
 
-  angular.module('tf2stadium').factory('$themeService', ThemeService);
+  angular.module('tf2stadium').factory('ThemeService', ThemeService);
 
   /** @ngInject */
   function ThemeService(Websocket) {
@@ -24,29 +24,40 @@
     var themeService = {};
 
     themeService.themes = [
-      {name: "TF2Stadium", selector: "default-theme", id: 0},
-      {name: "TF2Stadium Dark", selector: "dark-theme", id: 1}
+      {name: "TF2Stadium", selector: "default-theme", id: "0"},
+      {name: "TF2Stadium Dark", selector: "dark-theme", id: "1"}
     ]
 
     themeService.currentTheme = themeService.themes[0];
 
-    themeService.readThemeSettings = function () {
+    themeService.readThemeSettings = function() {
       Websocket.emit('playerSettingsGet',
         JSON.stringify({key: 'theme'}),
         function(data) {
           var response = JSON.parse(data);
-          themeService.currentTheme = JSON.parse(response.data.theme);
+          for (var i=0; i < themeService.themes.length; i++) {
+            var theme = themeService.themes[i];
+            if (theme.id == response.data.theme) {
+              themeService.currentTheme = theme;
+            }
+          }
         }
       );
     }
 
-    themeService.saveThemeSettings = function () {
+    themeService.saveThemeSettings = function() {
       Websocket.emit('playerSettingsSet',
-        JSON.stringify({key: 'theme', value: JSON.stringify(currentTheme)}),
+        JSON.stringify({key: 'theme', value: themeService.currentTheme.id}),
         function(data) {
           var response = JSON.parse(data);
+          console.log(response)
         }
       );
+    }
+
+    themeService.setCurrentTheme = function(theme) {
+      themeService.currentTheme = theme;
+      themeService.saveThemeSettings();
     }
 
     themeService.readThemeSettings();
@@ -56,17 +67,25 @@
   }
 
   angular.module('tf2stadium')
-  .controller('ThemeController', ['$scope', '$themeService', function($scope, $themeService) {
+  .controller('ThemeController', ['$scope', 'ThemeService', function($scope, themeService) {
 
-    $scope.themes = $themeService.themes;
-    $scope.currentTheme = $themeService.currentTheme;
+    $scope.themes = themeService.themes;
+
+    $scope.$watch(
+      function() {
+        return themeService.currentTheme;
+      },
+      function() {
+        $scope.currentTheme = themeService.currentTheme;
+      }
+    );
 
     $scope.setCurrentTheme = function(theme) {
-      $themeService.currentTheme = theme;
+      themeService.setCurrentTheme(theme);
     }
     
     $scope.getCurrentTheme = function() {
-      return $themeService.currentTheme;
+      return themeService.currentTheme;
     }
 
   }]);
