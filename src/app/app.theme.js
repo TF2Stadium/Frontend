896a95e0f9085e650449045d4c929c5bae7bff9b
Capu <mariokapu@gmail.com>
2015-08-30 19:04:30 +0200

    Added settings read support to $themeService.

--- src/app/app.theme.js
+++ src/app/app.theme.js
@@ -16,36 +16,57 @@
       .accentPalette('lightBluePalette');
   });
 
-  angular.module('tf2stadium').factory('themeService', function() {
+  angular.module('tf2stadium').factory('$themeService', ThemeService);
 
-    var themes = [
-      {name: "TF2Stadium", selector: "default-theme"},
-      {name: "TF2Stadium Dark", selector: "dark-theme"}
+  /** @ngInject */
+  function ThemeService(Websocket) {
+
+    var themeService = {};
+
+    themeService.themes = [
+      {name: "TF2Stadium", selector: "default-theme", id: 0},
+      {name: "TF2Stadium Dark", selector: "dark-theme", id: 1}
     ]
 
-    var currentTheme = themes[1];
+    themeService.currentTheme = themeService.themes[0];
 
-    return {
-      getThemes: function() {
-          return themes;
-        },
-      currentTheme: currentTheme
-    };
+    themeService.readThemeSettings = function () {
+      Websocket.emit('playerSettingsGet',
+        JSON.stringify({key: 'theme'}),
+        function(data) {
+          var response = JSON.parse(data);
+          themeService.currentTheme = JSON.parse(response.data.theme);
+        }
+      );
+    }
 
-  });
+    themeService.saveThemeSettings = function () {
+      Websocket.emit('playerSettingsSet',
+        JSON.stringify({key: 'theme', value: JSON.stringify(currentTheme)}),
+        function(data) {
+          var response = JSON.parse(data);
+        }
+      );
+    }
+
+    themeService.readThemeSettings();
+
+    return themeService;
+
+  }
 
   angular.module('tf2stadium')
-  .controller('ThemeController', ['$scope', 'themeService', function($scope, themeService) {
+  .controller('ThemeController', ['$scope', '$themeService', function($scope, $themeService) {
 
-    $scope.themes = themeService.getThemes();
-    $scope.currentTheme = themeService.currentTheme;
+    $scope.themes = $themeService.themes;
+    $scope.currentTheme = $themeService.currentTheme;
 
     $scope.setCurrentTheme = function(theme) {
-      themeService.currentTheme = theme;
+      $themeService.currentTheme = theme;
     }
     
     $scope.getCurrentTheme = function() {
-      return themeService.currentTheme;
+      return $themeService.currentTheme;
     }
 
   }]);
