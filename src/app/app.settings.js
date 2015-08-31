d567dd3207cbe79d4c2dcbce7de8c8f385b1e55e
Capu <mariokapu@gmail.com>
2015-08-31 17:03:50 +0200

    Added callbacks to Websocket responses.

--- src/app/app.settings.js
+++ src/app/app.settings.js
@@ -17,9 +17,13 @@
   /** @ngInject */
   function SettingsRunBlock(Websocket, Settings) {
     console.log('Setting test after config: ' + Settings.get("test"));
-    Settings.set("test", "runBlock");
+    Settings.set("test", "runBlock", function() {
+      console.log('Callback from set!');
+    });
     console.log('Setting test after runblock: ' + Settings.get("test"));
-    Settings.loadSettings();
+    Settings.loadSettings(function() {
+      console.log('Callback from loadSettings!');
+    });
   }
 
   //Provider configuration
@@ -32,13 +36,18 @@
 
 
     /*
-        Creates the service with all the functions accessible
-        during and after the runBlock
+      Creates the service with all the functions accessible
+      during and after the run phase.
     */
     var settingsService = function(Websocket) {
 
-      settingsService.set = function(key, value) {
+      /*
+        Saves a setting into the service and into the backend and
+        fires an optional callback with the response from the backend as an argument.
+      */
+      settingsService.set = function(key, value, callback) {
 
+        callback = callback || angular.noop;
         settings[key] = value;
 
         Websocket.emit('playerSettingsSet',
@@ -48,6 +57,7 @@
             if (response.success) {
               console.log('Setting "' + key + '" saved correctly on the backend!');
             }
+            callback(response);
           }
         );
       }
@@ -56,7 +66,14 @@
         return settings[key];
       }
 
-      settingsService.loadSettings = function() {
+      /*
+        Loads all settings, saves them into the service in case of success and
+        fires an optional callback with the response from the backend as an argument.
+      */
+      settingsService.loadSettings = function(callback) {
+
+        callback = callback || angular.noop;
+
         Websocket.emit('playerSettingsGet',
           JSON.stringify({key: ''}),
           function(data) {
@@ -65,6 +82,7 @@
               settings = response.data;
               console.log('Settings loaded correctly! ---> ' + JSON.stringify(response.data));
             }
+            callback(response);
           }
         );
       }
@@ -73,10 +91,10 @@
     };
 
     /*
-        Creates the service with all the objects and functions
-        accessible ONLY DURING config phase.
+      Creates the service with all the objects and functions
+      accessible ONLY DURING config phase.
 
-        $get returns the service object
+      $get returns the service object.
     */
 
     var settingsProvider = {
