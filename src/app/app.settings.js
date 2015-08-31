66cf7ac25ff931cc6226a1522ef8713fcf404d95
Capu <mariokapu@gmail.com>
2015-08-31 16:31:28 +0200

    Changed settings service to a provider.

--- src/app/app.settings.js
+++ src/app/app.settings.js
@@ -1,24 +1,75 @@
 (function() {
   'use strict';
 
-  angular.module('tf2stadium').factory('SettingsService', SettingsService);
+  angular
+  .module('tf2stadium')
+  .provider('Settings', Settings)
+  .config(SettingsConfigBlock)
+  .run(SettingsRunBlock);
 
   /** @ngInject */
-  function SettingsService(Websocket) {
+  function SettingsConfigBlock(SettingsProvider) {
+    SettingsProvider.settings["test"] = "config";
+  }
 
-    console.log('Starting SettingsService');
-    var settingsService = {};
+  /** @ngInject */
+  function SettingsRunBlock(Websocket, Settings) {
+    console.log('Setting test after config: ' + Settings.get("test"));
+    Settings.set("test", "runBlock");
+    console.log('Setting test after runblock: ' + Settings.get("test"));
+    Settings.loadSettings();
+  }
 
-    settingsService.save = function() {
+  /** @ngInject */
+  function Settings() {
+    console.log('Starting Settings');
 
+    var settings = {"test": "init"};
+    console.log('Setting test initialized: ' + settings['test']);
+
+    var settingsService = function(Websocket) {
+
+      settingsService.set = function(key, value) {
+
+        settings[key] = value;
+
+        Websocket.emit('playerSettingsSet',
+          JSON.stringify({key: key, value: value}),
+          function(data) {
+            var response = JSON.parse(data);
+            if (response.success) {
+              console.log('Setting "' + key + '" saved correctly on the backend!');
+            }
+          }
+        );
+      }
+
+      settingsService.get = function(key) {
+        return settings[key];
+      }
+
+      settingsService.loadSettings = function() {
+        Websocket.emit('playerSettingsGet',
+          JSON.stringify({key: ''}),
+          function(data) {
+            var response = JSON.parse(data);
+            if (response.success) {
+              settings = response.data;
+              console.log('Settings loaded correctly! ---> ' + JSON.stringify(response.data));
+            }
+          }
+        );
+      }
+
+      return settingsService;
+    };
+
+    var settingsProvider = {
+      settings: settings,
+      $get: settingsService
     }
 
-    settingsService.load = function() {
-
-    }
-
-    return settingsService;
-
+    return settingsProvider;
   }
 
 })();
