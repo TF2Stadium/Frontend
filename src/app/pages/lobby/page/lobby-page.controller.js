dd219920a77ce66ba550cd297d0a48693700ff9d
Daniel Schalla <daniel@schalla.me>
2015-09-18 00:37:42 +0200

    Added Header Data and Debug Data for Lobby Page

--- src/app/pages/lobby/page/lobby-page.controller.js
+++ src/app/pages/lobby/page/lobby-page.controller.js
@@ -26,57 +26,20 @@
       Websocket.emit('lobbyJoin', JSON.stringify(lobbyData), function(data) {
         var response = JSON.parse(data);
         //ToDo: Error Handling
-        if (response.success === true) {
+        if (response.success !== true) {
         }
       });
     };
 
+    Websocket.on('lobbyReadyUp', function(data) {
+      console.log(data);
+    });
+
+    Websocket.on('lobbyStart', function(data) {
+      console.log(data);
+    });
+
   }
 
-  /**
-  function LobbyPageController() {
-    var vm = this;
-
-    vm.lobbyInformation={
-        type: 'hl',
-        map: 'pl_upward',
-        classes: [
-          {classname: 'scout', slots:{
-              blu: {name:'Crayboff', hoursPlayed:123, lobbiesPlayed:123, reliability:99, id:'1234'},
-              red: {name:'Red', hoursPlayed:123, lobbiesPlayed:123, reliability:99, id:''}
-          }},
-          {classname: 'scout', slots:{
-              blu: {name:'Blu', hoursPlayed:123, lobbiesPlayed:123, reliability:99, id:''},
-              red: {name:'Schalla', hoursPlayed:123, lobbiesPlayed:123, reliability:99, id:'25'}
-          }},
-          {classname: 'soldier', slots:{
-              blu: {name:'Capu', hoursPlayed:123, lobbiesPlayed:123, reliability:99, id:'1234'},
-              red: {name:'Other player', hoursPlayed:123, lobbiesPlayed:123, reliability:99, id:'234'}
-          }},
-          {classname: 'soldier', slots:{
-              blu: {name:'Masternoob', hoursPlayed:123, lobbiesPlayed:123, reliability:99, id:'1234'},
-              red: {name:'Red', hoursPlayed:123, lobbiesPlayed:123, reliability:99, id:''}
-          }},
-          {classname: 'demoman', slots:{
-              blu: {name:'Blu', hoursPlayed:123, lobbiesPlayed:123, reliability:99, id:''},
-              red: {name:'Red', hoursPlayed:123, lobbiesPlayed:123, reliability:99, id:''}
-          }},
-          {classname: 'medic', slots:{
-              blu: {name:'Gibusvision', hoursPlayed:123, lobbiesPlayed:123, reliability:99, id:'1234'},
-              red: {name:'Red', hoursPlayed:123, lobbiesPlayed:123, reliability:99, id:''}
-          }},
-        ],
-        currentSlotsTaken: 9,
-        region: 'eu',
-        mumbleRequired: true,
-        rules:'UGC',
-        serverLocation: 'Germany',
-        owner: 'Lobby leader',
-        spectators: [
-          {name:'Spectator 1'},
-          {name:'Another spec'}
-        ]
-    };
-  }*/
 
 })();
