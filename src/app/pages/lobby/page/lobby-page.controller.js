112026a96e33eab1038f409d9f6384d4a75424e5
Daniel Schalla <daniel@schalla.me>
2015-09-20 22:04:24 +0200

    Added Lobby Ready Up Popup

--- src/app/pages/lobby/page/lobby-page.controller.js
+++ src/app/pages/lobby/page/lobby-page.controller.js
@@ -3,10 +3,11 @@
 
   angular
     .module('tf2stadium')
-    .controller('LobbyPageController', LobbyPageController);
+    .controller('LobbyPageController', LobbyPageController)
+    .controller('LobbyPageReadyDialog', LobbyPageReadyDialog);
 
   /** @ngInject */
-  function LobbyPageController($scope, LobbyService, Websocket) {
+  function LobbyPageController($scope, LobbyService, Websocket, $mdDialog) {
     var vm = this;
 
     vm.lobbyInformation = LobbyService.getActive();
@@ -31,12 +32,54 @@
     };
 
     Websocket.onJSON('lobbyReadyUp', function(data) {
-      console.log(data);
+      $mdDialog.show({
+        templateUrl: 'lobbypage-readyup.html.tpl',
+        controller: 'LobbyPageReadyDialog',
+        controllerAs: 'dialog'
+      })
+      .then(function(response) {
+        if (response === "ready") {
+          Websocket.emit("playerReady");
+        } else {
+          var payload = {
+            "id": vm.lobbyInformation.id
+          };
+          Websocket.emitJSON("lobbyKick", payload);
+        }
+      });
     });
 
     Websocket.onJSON('lobbyStart', function(data) {
       console.log(data);
+      alert("STARTED");
     });
+
+  }
+
+  function LobbyPageReadyDialog($mdDialog, $timeout) {
+    var vm = this;
+
+    vm.seconds = 0;
+    vm.limit = 30;
+
+    vm.increaseCounter = function(){
+      vm.seconds++;
+      timer = $timeout(vm.increaseCounter,1000);
+
+      if (vm.seconds > vm.limit) {
+        $mdDialog.hide('leave');
+      }
+    };
+
+    var timer = $timeout(vm.increaseCounter,1000);
+
+    vm.ready = function () {
+      $mdDialog.hide('ready');
+    };
+
+    vm.leave = function () {
+      $mdDialog.hide('leave');
+    };
 
   }
 
