0ccee0ce048dadb0ea489aa93a341e47d58041a1
Capu <mariokapu@gmail.com>
2015-09-20 20:33:43 +0200

    Forbid direct navigation to child states. Redirect to parent.

--- src/app/app.run.js
+++ src/app/app.run.js
@@ -5,12 +5,18 @@
 
   /** @ngInject */
 
-  function runBlock($log, $rootScope, Config, ThemeService) {
+  function runBlock($log, $rootScope, $state, Config, ThemeService) {
     $log.debug('runBlock end');
 
     $rootScope.$on('$stateChangeStart',
-      function(event, toState){
+      function(event, toState, toParams, fromState) {
         $rootScope.currentState = toState;
+
+        //Forbid direct navigation to children states
+        if (!fromState.name && toState.parent) {
+          event.preventDefault();
+          $state.go(toState.parent, toParams);          
+        }
       }
     );
 
