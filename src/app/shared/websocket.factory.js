957927f8832effffc619740ece991b4084bd5119
Capu <mariokapu@gmail.com>
2015-09-27 20:47:47 +0200

    Websocket: added null callback check.

--- src/app/shared/websocket.factory.js
+++ src/app/shared/websocket.factory.js
@@ -13,24 +13,27 @@
     });
 
     factory.onJSON = function(name, callback) {
+      callback = callback || angular.noop;
+
       factory.on(name, function (data) {
-
         var json = {};
-
         if (typeof(json) !== "undefined" && data !== "") {
           json = JSON.parse(data);
         }
-
         callback(json);
       });
+      
     };
 
     factory.emitJSON = function(name, data, callback) {
+      callback = callback || angular.noop;
       var json = JSON.stringify(data);
-      factory.emit(name, json, function(json){
+      
+      factory.emit(name, json, function(json) {
         var data = JSON.parse(json);
         callback(data);
       });
+
     };
 
     return factory;
