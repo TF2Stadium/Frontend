eef789f7a80336ae16bb728cab6b8231f87bbfaa
Capu <mariokapu@gmail.com>
2015-09-28 18:42:43 +0200

    Fix: commit toast files.

---
+++ src/app/shared/notifications/toast.controller.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+  var app = angular.module('tf2stadium');
+  app.controller('ToastController', ToastController);
+
+  /** @ngInject */
+  function ToastController($mdToast) {
+    var vm = this;
+
+    vm.hide = function() {
+      $mdToast.hide();
+    }
+    
+  }
+
+})();
