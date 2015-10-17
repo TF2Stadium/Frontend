7797369469f4ab7e244eb7651216417d22024228
Capu <mariokapu@gmail.com>
2015-10-17 19:18:36 +0200

    User service: refactored code.

--- src/app/shared/user.factory.js
+++ src/app/shared/user.factory.js
@@ -12,25 +12,42 @@
     var userProfile = {};
     var alreadyLoadedFromBackend = false;
 
-    userService.getProfile = function(callback) {
+    userService.getUserProfile = function(callback) {
 
       callback = callback || angular.noop;
 
-      if (alreadyLoadedFromBackend) {
+      if (!alreadyLoadedFromBackend) {
+        userService.getProfile ('', function(response) {
+          if (response.success) {
+            userProfile = response.data;
+          }
+          callback(response.data);
+        });
+      } else {
         callback(userProfile);
-        return userProfile;
       }
 
-      getProfileFromBackend(function() {
-        callback(userProfile);
-      });
       return userProfile;
+
+    };
+
+    userService.getProfile = function(steamid, callback) {
+
+      callback = callback || angular.noop;
+
+      Websocket.emitJSON('playerProfile',
+        {steamid: steamid},
+        function(response) {
+          callback(response.data);
+        }
+      );
 
     };
 
     userService.init = function() {
       Websocket.onJSON('playerProfile', function(data) {
         $rootScope.userProfile = data;
+        alreadyLoadedFromBackend = true;
       });
     };
 
