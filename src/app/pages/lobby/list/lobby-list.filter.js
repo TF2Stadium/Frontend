01c5ef03f140272283d1ac885c9e2131481f4e34
gpittarelli <tf2stadium@gjp.cc>
2015-11-01 21:02:43 -0500

    Add filtering lobbies by available classes

--- src/app/pages/lobby/list/lobby-list.filter.js
+++ src/app/pages/lobby/list/lobby-list.filter.js
@@ -5,6 +5,28 @@
     .module('tf2stadium.services')
     .filter('LobbyListSettingsFilter', LobbyListSettingsFilter);
 
+  var CLASS_SYNONYMS = {
+    roamer: 'soldier',
+    pocket: 'soldier',
+    scout1: 'scout',
+    scout2: 'scout'
+  };
+
+  function maybeClassName(slot) {
+    if (!(slot.blu.filled && slot.red.filled)) {
+      var className = slot.class;
+      if (CLASS_SYNONYMS.hasOwnProperty(className)) {
+        className = CLASS_SYNONYMS[className];
+      }
+      return className;
+    }
+    return false;
+  }
+
+  function truthy(x) {
+    return !!x;
+  }
+
   /** @ngInject */
   function LobbyListSettingsFilter(Settings, Config) {
 
@@ -20,17 +42,22 @@
         var lobby = lobbies[key];
         lobby.region = "eu";
 
-        if (settings[lobby.region] &&
-          settings[lobby.type] &&
-          settings[lobby.map.substr(0, lobby.map.indexOf('_'))]
+        var availableClasses = lobby.classes.map(maybeClassName).filter(truthy);
 
-          || (Config.debug && lobby.type === 'Debug')) {
+        if (settings[lobby.region] &&
+            settings[lobby.type] &&
+            settings[lobby.map.substr(0, lobby.map.indexOf('_'))] &&
+            availableClasses.some(function(className) {
+              return settings[className];
+            })
+
+            || (Config.debug && lobby.type === 'Debug')) {
           filteredList[key] = lobby;
         }
       }
 
       return filteredList;
     };
-    
+
   }
 })();
