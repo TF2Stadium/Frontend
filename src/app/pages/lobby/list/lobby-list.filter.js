bf588ad1b37201ff396a9ed66433ec9e98929ba8
Capu <mariokapu@gmail.com>
2015-11-02 19:53:10 +0100

    Fix bad merge conflict resolution.

--- src/app/pages/lobby/list/lobby-list.filter.js
+++ src/app/pages/lobby/list/lobby-list.filter.js
@@ -46,17 +46,11 @@
               .filter(truthy);
 
         if (settings[lobby.region] &&
-<<<<<<< HEAD
             settings[lobby.type] &&
             settings[lobby.map.substr(0, lobby.map.indexOf('_'))] &&
             availableClasses.some(playerPlaysClass)
 
             || (Config.debug && lobby.type === 'Debug')) {
-=======
-          settings[lobby.type] &&
-          settings[lobby.map.substr(0, lobby.map.indexOf('_'))] ||
-          (Config.debug && lobby.type === 'Debug')) {
->>>>>>> 9085363ca17429d83dbc672a23bbe7e20023a939
           filteredList[key] = lobby;
         }
       }
