44f72ccc170eb038c43a800be137fd09ad742480
gpittarelli <tf2stadium@gjp.cc>
2015-11-06 13:05:38 -0500

    upgrade to wsevent.js 0.0.3, with reconnecting in that lib

--- src/app/shared/websocket.factory.js
+++ src/app/shared/websocket.factory.js
@@ -7,7 +7,6 @@
   function Websocket(Config, Notifications, $rootScope, $timeout) {
     var connected = false;
     var socket = null;
-    var messageHandler = null;
 
     var asyncAngularify = function (callback) {
       return function () {
@@ -18,80 +17,118 @@
       };
     };
 
-    function connect() {
-      socket = new WebSocket(Config.endpoints.websocket);
-      messageHandler = new Socket(socket, extractor);
+    socket = new Socket(Config.endpoints.websocket,
+                        {extractor: extractor});
 
-      socket.onopen = function (e) {
-        // Note: connected=true must come before we emit the event,
-        // otherwise listeners might try to send data in response to the
-        // event and it will never get sent (because when
-        // connected==false, data gets queued for the socket-opened
-        // event).
-        connected = true;
-        asyncAngularify(function () {
-          $rootScope.$emit('socket-opened');
-        });
-        console.log('WebSocket connection opened', e);
-      };
+    socket.onopen = function (e) {
+      // Note: connected=true must come before we emit the event,
+      // otherwise listeners might try to send data in response to the
+      // event and it will never get sent (because when
+      // connected==false, data gets queued for the socket-opened
+      // event).
+      connected = true;
+      asyncAngularify(function () {
+        $rootScope.$emit('socket-opened');
+      })();
+      console.log('WebSocket connection opened', e);
+    };
 
-      socket.onclose = function (e) {
+    socket.onclose = function (e) {
+      asyncAngularify(function () {
         $rootScope.$emit('socket-closed');
-        console.log('WebSocket closed:', e);
-        connected = false;
-        connect();
-      };
-
-      socket.onerror = function (e) {
-        console.log('WebSocket error:', e);
-      };
-    }
-
-    connect();
+      })();
+      console.log('WebSocket closed:', e);
+      connected = false;
+    };
 
     function extractor(data) {
       return data.request;
     }
 
-    function emitJSON(name, data, callback) {
-      console.log('Sending ' + name, data);
-      data.request = name;
+    // Hack to work around our shitty app state handling: we want to
+    // start the socket as soon as possible, and the server also sends
+    // state initialization data as soon as possible, but that means
+    // it is possible (and has been observed) for us to receive that
+    // initialization data sent back before the proper handlers have
+    // been registered.
+    //
+    // To account for this, we do an extra layer of event handling to
+    // queue up messages and send them to event handlers that get
+    // registered too slowly as they are registered.
+    //
+    // The same thing is done in .emitJSON for messages being sent out
+    // before the socket is connected.
+    var queuedMessages = Object.create(null);
+    var registeredHandlers = Object.create(null);
+    socket.onmessage = function(name, data) {
+      if (!registeredHandlers[name]) {
+        console.log('Received message with no registered handler: ' + name,
+                    data);
+        if (!queuedMessages[name]) {
+          queuedMessages[name] = [];
+      }
 
-      messageHandler.Emit(data, function(jsonIn) {
-        var data = JSON.parse(jsonIn);
-        console.log('Response to ' + name, data);
-        console.log(data);
-        if (!data.success) {
-          Notifications.toast({message: data.message, error: true});
-        }
-        callback(data);
-      });
+      queuedMessages[name].push(data);
+    }
+  };
+
+  var factory = {};
+  factory.onJSON = function(name, callback) {
+    callback = asyncAngularify(callback || angular.noop);
+
+    // Dispatch queued messages for the initialization
+    // workaround. Technically it is possible for messages to arrive
+    // out of order by doing this, but it should be negligible (a
+    // message would have to arrive and be delivered to the socket
+    // between when this function completes and the 0-delay timeout of
+    // asyncAngularify). We can't skip the timeout here because event
+    // handlers are not generally defined with the intention of being
+    // called before their enclosing scope finishes executing.
+    registeredHandlers[name] = true;
+    if (queuedMessages[name]) {
+      asyncAngularify(function () {
+        queuedMessages[name].forEach(function (data) {
+          console.log('Received: ' + name, data);
+          callback(data);
+        });
+      })();
     }
 
-    var factory = {};
-    factory.onJSON = function(name, callback) {
-      console.log("registered: " + name);
-      callback = asyncAngularify(callback || angular.noop);
+    socket.On(name, function (data) {
+      console.log('Received: ' + name, data);
+      callback(data);
+    });
+  };
 
-      messageHandler.On(name, function (data) {
-        console.log('Received ' + name, data);
-        callback(data);
-      });
-    };
+  function emitJSON(name, data, callback) {
+    console.log('Sending ' + name, data);
+    data.request = name;
 
-    factory.emitJSON = function(name, data, callback) {
-      callback = asyncAngularify(callback || angular.noop);
-
-      if (connected) {
-        emitJSON(name, data, callback);
-      } else {
-        $rootScope.$on('socket-opened', function() {
-          emitJSON(name, data, callback);
-        });
+    socket.Emit(data, function(jsonIn) {
+      var data = JSON.parse(jsonIn);
+      console.log('Response to ' + name, data);
+      console.log(data);
+      if (!data.success) {
+        Notifications.toast({message: data.message, error: true});
       }
-    };
-
-    return factory;
+      callback(data);
+    });
   }
+
+  factory.emitJSON = function(name, data, callback) {
+    callback = asyncAngularify(callback || angular.noop);
+
+    if (connected) {
+      emitJSON(name, data, callback);
+    } else {
+      var deregister = $rootScope.$on('socket-opened', function() {
+        emitJSON(name, data, callback);
+        deregister();
+      });
+    }
+  };
+
+  return factory;
+}
 
 })();
