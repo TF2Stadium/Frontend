32f72241a785901f51155d208b3007d7c9ca3146
Capu <mariokapu@gmail.com>
2015-11-26 19:21:10 +0100

    Moved spectated lobby leaving logic to LobbyService.

--- src/app/pages/lobby/list/lobby-list.controller.js
+++ src/app/pages/lobby/list/lobby-list.controller.js
@@ -14,10 +14,6 @@
       vm.lobbies = LobbyService.getList();
     });
 
-    // When we navigate to the main lobby list page, leave the
-    // spectator spot for whatever lobby we were just in.
-    LobbyService.leaveSpectatedLobby();
-
     vm.join = function (lobby, team, position, event) {
       event.preventDefault();
       event.stopImmediatePropagation();
