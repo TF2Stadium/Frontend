515e3904f9693ad815329de2fae8c302d1cc691b
gpittarelli <tf2stadium@gjp.cc>
2015-12-17 04:01:16 -0500

    testing: run without needing to build
    requires npm install

--- gulp/test.js
+++ gulp/test.js
@@ -10,26 +10,38 @@ var karma = require('karma');
 var $ = require('gulp-load-plugins')();
 
 gulp.task('test:unit', function (done) {
-  var server = new karma.Server({
-    browsers: ['PhantomJS'],
-    frameworks: ['mocha', 'chai-sinon'],
-    files: mainBowerFiles({ includeDev: true }).concat([
-      'dist/scripts/app-*.js',
-      'test/karma/**/*.js'
-    ]),
-    logLevel: 'DEBUG',
-    singleRun: true
-  });
+  var src_files = [];
+  gulp.src(path.join(conf.paths.src, '/app/**/*.js'))
+    .pipe($.angularFilesort())
+    .on('data', function (file) {
+      src_files.push(file.path);
+    }).on('end', function () {
+      var server = new karma.Server({
+        browsers: ['PhantomJS'],
+        frameworks: ['mocha', 'chai-sinon'],
+        files:
+        mainBowerFiles({ includeDev: true })
+          .concat(
+            // Note: es5-shim needed to fix some phantomjs issues,
+            // including no Function.prototype.bind . This can be
+            // removed (along with the es5-shim dependency) once an
+            // upgrade to phantomjs2 is complete
+            'node_modules/es5-shim/es5-shim.js',
+            src_files,
+            'test/karma/**/*.js'),
+        logLevel: 'DEBUG',
+        singleRun: true
+      });
 
-  server.on('run_complete', function (browsers, results) {
-    // NB If the argument of done() is not null or not undefined,
-    // e.g. a string, the next task in a series won't run.
-    done(results.error ? 'There are test failures' : null);
-  });
-
-  server.start();
+      server.on('run_complete', function (browsers, results) {
+        // NB If the argument of done() is not null or not undefined,
+        // e.g. a string, the next task in a series won't run.
+        done(results.error ? 'There are test failures' : null);
+      });
+      server.start();
+    });
 });
 
 gulp.task('test', function (cb) {
-  runSequence('build', 'test:unit', cb);
+  runSequence('test:unit', cb);
 });
