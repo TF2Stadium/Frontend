eeb822d63b0d172bf7c1cf7c994a976e68d03c78
gpittarelli <tf2stadium@gjp.cc>
2015-12-21 20:26:20 -0500

    try a console.trace

--- window.js
+++ window.js
@@ -3,5 +3,6 @@ console.profile('cause of reload');
 window.addEventListener('beforeunload', function() {
   console.profileEnd('cause of reload');
   console.error('TEST FAILEDDDD SOMETHING TRIGGERED UNLOAD');
+  console.trace('beforeunload');
   throw new Error('beforeunload happened');
 });
