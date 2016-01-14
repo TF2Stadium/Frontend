7cc7c0f684ac2ea54ff4ca27895b9fbdedc273f2
gpittarelli <tf2stadium@gjp.cc>
2016-01-13 21:19:51 -0500

    Add emojione config generation script

---
+++ util/emojione-config/fetch.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+var https = require('https');
+
+const URL = 'https://raw.githubusercontent.com/Ranks/emojione/master/emoji.json';
+
+https.get(URL)
+  .on('response', function handle(response) {
+    var msg = '';
+    response.setEncoding('utf8');
+
+    response.on('data', (chunk) => {msg += chunk;});
+    response.on('end', () => writeConfig(JSON.parse(msg)));
+  });
+
+// :abc: -> abc
+function removeColons(str) {
+  return str.substring(1, str.length - 1);
+}
+
+function writeConfig(rawEmojis) {
+  var config = Object.keys(rawEmojis).filter(function (emojiName) {
+    var emoji = rawEmojis[emojiName];
+    return emoji.category === 'flags' || emoji.category === 'people';
+  }).map(function (emojiName) {
+    var emoji = rawEmojis[emojiName];
+
+    return {
+      names: emoji['aliases'].map(removeColons).concat(emojiName),
+      shortcuts: emoji['aliases_ascii'],
+      image: {
+        type: 'img',
+        src: emoji['unicode'] + '.png'
+      }
+    };
+  });
+
+  process.stdout.write(JSON.stringify(config));
+}
