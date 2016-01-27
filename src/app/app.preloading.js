9445a8aa75bf234484f509fce9e4d240f6bfc9d6
gpittarelli <tf2stadium@gjp.cc>
2016-01-27 15:01:54 -0500

    cache preload requests
    Signed-off-by: gpittarelli <tf2stadium@gjp.cc>

--- src/app/app.preloading.js
+++ src/app/app.preloading.js
@@ -6,10 +6,21 @@
 
   /** @ngInject */
   function PreloadService($window) {
+    // Never attempt to preload the same image multiple times. This
+    // isn't a huge deal, since making multiple requests for the same
+    // image will get stopped by the cache, but if we're ever
+    // attempting to preload images that don't exist (mainly happens
+    // during development), we don't end up spamming requests for that
+    // image (since 404s aren't cached).
+    var alreadyQueued = Object.create(null);
+
     return {
       queuePreload: function preload(src) {
-        var img = new $window.Image();
-        img.src = src;
+        if (!alreadyQueued[src]) {
+          var img = new $window.Image();
+          img.src = src;
+          alreadyQueued[src] = true;
+        }
       }
     };
   }
