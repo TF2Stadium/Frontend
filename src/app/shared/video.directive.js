59797bc05f05218aed91d8c3f362060ae07cb814
gpittarelli <tf2stadium@gjp.cc>
2016-02-23 17:42:17 -0500

    cinemagraph: unload when window isn't focused
    This is to address performance concerns when palyers have the
    cinemagraph enabled, but focus another window (say, TF2).
    
    Signed-off-by: gpittarelli <tf2stadium@gjp.cc>

--- src/app/shared/video.directive.js
+++ src/app/shared/video.directive.js
@@ -6,11 +6,16 @@
 
   /** @ngInject */
   function VideoBackgroundDirective($rootScope, Settings) {
+    var focused = Kefir.merge([
+      Kefir.fromEvents(window, 'focus').map(function () { return true; }),
+      Kefir.fromEvents(window, 'blur').map(function () { return false; })
+    ]);
+
     return {
       restrict: 'E',
       scope: {},
       template:
-        '<video ng-if="showVideo" ng-show="videoReady" ng-canplay="canPlay()" autoplay autostart loop class="cinemagraph">' +
+        '<video ng-if="showVideo && pageFocused" ng-show="videoReady" ng-canplay="canPlay()" autoplay autostart loop class="cinemagraph">' +
         '<source src="/assets/video/koth_suijin.vp9.webm" type=\'video/webm;codecs="vp9"\' />'+
         '<source src="/assets/video/koth_suijin.vp8.webm" type=\'video/webm;codecs="vp8"\' />' +
         '<source src="/assets/video/koth_suijin.h264.mp4" type="video/mp4" />' +
@@ -18,8 +23,19 @@
 
       link: function (scope) {
         scope.showVideo = false;
+        scope.pageFocused = true;
 
-        // used via ng-show to prevent a black flash from displaying
+        focused.onValue(function (isFocused) {
+          scope.$apply(function () {
+            if (!scope.pageFocused && isFocused) {
+              scope.videoReady = false;
+            }
+
+            scope.pageFocused = isFocused;
+          });
+        });
+
+        // use ng-show to prevent a black flash from displaying
         // an unloaded video when the videoBackground setting changes
         scope.videoReady = false;
         scope.canPlay = function () {
