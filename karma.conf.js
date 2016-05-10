a3dc8408b17c4eb6277c9e02e46b28daad834091
George Pittarelli <g@gjp.cc>
2016-05-10 09:32:11 -0700

    allow requiring videos in karma tests
    Signed-off-by: George Pittarelli <g@gjp.cc>

--- karma.conf.js
+++ karma.conf.js
@@ -58,7 +58,7 @@ module.exports = function (config) {
             'svgo',
           ],
         }, {
-          test: /\.(png|jpg|jpeg|gif|woff|woff2|ttf|eot|otf)$/,
+          test: /\.(png|jpg|jpeg|gif|woff|woff2|ttf|eot|otf|webm|mp4)$/,
           loader: 'file?name=[path][name].[ext]',
         }, {
           test: /\.js$/,
