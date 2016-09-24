56ab385d73c4f1980d8c0f46d4f3c5032bf142f5
gpittarelli <tf2stadium@gjp.cc>
2016-09-23 23:36:21 -0700

    change __BUILD_STATS__ declaration
    works around a weird eslint/babel with flow bug

--- src/app/build-stats.js
+++ src/app/build-stats.js
@@ -1,5 +1,5 @@
 // @flow
 import type { BuildStats } from '../../headers/BuildStats';
 
-declare var __BUILD_STATS__: BuildStats;
-export default __BUILD_STATS__;
+declare var __BUILD_STATS__;
+export default (__BUILD_STATS__: BuildStats);
